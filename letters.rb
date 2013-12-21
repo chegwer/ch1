b30c6723026c834a68ac5ea613dfdd7c7837fb45
Courtney Hegwer <thegwer@hotmail.com>
2013-12-21 13:54:09 -0600

    southpark meets got

--- letters.rb
+++ letters.rb
@@ -1,6 +1,14 @@
 puts 'Hello, world!'
 puts ''
 puts 'Good-bye.'
-
+puts ' '
 puts ' rainbows and butterflies '
 puts ' i thought it was the compromise '
+
+puts 'String Concatenation'
+
+puts ' '
+
+puts 'I like' + ' apple pie.'
+puts '  '
+puts 'weiners ' * 4
