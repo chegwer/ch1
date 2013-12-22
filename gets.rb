db9e360ae21abed796295043ae35a3d7a23ca8cc
Courtney Hegwer <thegwer@hotmail.com>
2013-12-22 14:29:39 -0600

    name game

--- gets.rb
+++ gets.rb
@@ -1,5 +1,20 @@
-puts 'Do you have a name?'
-name = gets
+puts 'Say your name.'
+name = gets.chomp
 puts 'That is a really dumb name, ' + name
 puts ''
 puts 'srsly lol'
+
+puts ''
+puts 'What is your first name? '
+firnm = gets.chomp
+puts 'k'
+puts ''
+puts 'What is your middle name? '
+midnm = gets.chomp
+puts 'rite'
+puts ''
+puts 'What is your last name? '
+lasnm = gets.chomp
+puts 'uh huh'
+puts ''
+puts 'You are lame ' + firnm + ' ' + midnm + ' ' + lasnm + '.'
