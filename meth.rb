5950edccbadceee3c7ca4c22e6c42d5add9bb9e7
Courtney Hegwer <thegwer@hotmail.com>
2013-12-23 14:50:21 -0600

    mean boss now

--- meth.rb
+++ meth.rb
@@ -1,12 +1,18 @@
+
 puts 'First name: '
 finm = gets.chomp
-puts ''
 puts 'Middle name: '
 minm = gets.chomp
-puts ''
 puts 'Last name: '
 lanm = gets.chomp
 puts ''
 puts 'Your name ' + finm + ' ' + minm + " " + lanm + ", "
 longform = finm.length + minm.length + lanm.length
 puts 'has ' + longform.to_s + ' letters.'
+
+puts ''
+puts ''
+puts ''
+puts 'WHADDAYA WANT? '
+answer = gets.chomp
+puts 'WHADDAYA MEAN "' + answer + '" ?!? YOU\'RE FIRED!!'
