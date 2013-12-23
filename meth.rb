5ec2637091bad58a7e8a59499059ef9a2d1e6127
Courtney Hegwer <thegwer@hotmail.com>
2013-12-23 10:31:22 -0600

    that challenge wit numeros

--- meth.rb
+++ meth.rb
@@ -1,3 +1,12 @@
-vur = 'look at me i\'m so fancy and free'
-puts vur
-puts vur.reverse
+puts 'First name: '
+finm = gets.chomp
+puts ''
+puts 'Middle name: '
+minm = gets.chomp
+puts ''
+puts 'Last name: '
+lanm = gets.chomp
+puts ''
+puts 'Your name ' + finm + ' ' + minm + " " + lanm + ", "
+longform = finm.length + minm.length + lanm.length
+puts 'has ' + longform.to_s + ' letters.'
