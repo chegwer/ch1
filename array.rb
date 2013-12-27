389e0b35cf69f5007f1865619c391ee9a6fce38f
Courtney Hegwer <thegwer@hotmail.com>
2013-12-26 18:32:38 -0600

    i can say so many things

--- array.rb
+++ array.rb
@@ -5,3 +5,20 @@ names.each do |pam|
 	puts 'I hate ' + pam + '!!'
 	puts 'Kill them all.'
 end
+
+5.times do
+	puts 'LASAGNA'
+end
+
+puts names
+puts
+puts names.join(', ')
+
+puts
+puts names.join(' :( ') + ' ;P '
+
+faves = []
+faves.push 'timeshares'
+faves.push 'lima beans'
+puts faves.length
+puts faves
